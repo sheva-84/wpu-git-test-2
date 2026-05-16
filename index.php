18200cf947e256ddc5b386f982b3fb5b9440c0c3
sheva-84 <andrian.sheva@gmail.com>
2026-05-16 22:20:17 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio quae non dolorem temporibus asperiores beatae, corporis debitis neque et iusto necessitatibus similique eius fugit mollitia natus perferendis ab autem sunt?</p>
+    <ul>
+         <li>satu</li>
+         <li>dua</li>
+    </ul>
 </body>
 </html>
